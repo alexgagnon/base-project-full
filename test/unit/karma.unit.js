d8bb4ab7f9199c40795a2f39d098bdf5ee3439eb
alexgagnon <asgagnon@hotmail.com>
2018-07-16 13:58:51 -0400

    also test on phantomjs

--- test/unit/karma.unit.js
+++ test/unit/karma.unit.js
@@ -3,12 +3,13 @@ var base = require('./karma.base.js');
 module.exports = function(config) {
   config.set(
     Object.assign(base, {
-      browsers: ['Chrome', 'Firefox'],
+      browsers: ['Chrome', 'Firefox', 'PhantomJS'],
       reporters: ['progress'],
       singleRun: true,
       plugins: base.plugins.concat([
         'karma-chrome-launcher',
-        'karma-firefox-launcher'
+        'karma-firefox-launcher',
+        'karma-phantomjs-launcher'
       ])
     })
   );
